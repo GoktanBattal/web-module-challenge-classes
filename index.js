d02f98b6d0bf2205822ad4e0e83bb1e8ebc43c5a
GoktanBattal <goktanbattal@gmail.com>
2022-09-16 16:33:27 +0300

    first 4  questions are done

--- index.js
+++ index.js
@@ -116,7 +116,14 @@ class Car {
 */
 
 class Lambdasian {
-  
+  constructor ({name, age, location}){
+    this.name = name;
+    this.age = age;
+    this.location = location;
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location} `
+  }
 }
 
 /*
@@ -134,7 +141,21 @@ class Lambdasian {
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
 
-class Instructor {
+class Instructor extends Lambdasian{
+  constructor({name, age, location, specialty, favLanguage, catchPhrase}){
+  super ({name, age, location, specialty, favLanguage, catchPhrase});
+  this.specialty = specialty;
+  this.favLanguage = favLanguage;
+  this.catchPhrase = catchPhrase;}
+
+  demo(subject){
+    return `Today we are learning about ${subject}`
+};
+
+grade(student, subject){
+  return `${student.name} receives a perfect score on ${subject}`
+
+};
 
 }
 
